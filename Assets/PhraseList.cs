35eba9b32415d5620c109af5fcf50b342a9a2866
Blananas2 <45607696+Blananas2@users.noreply.github.com>
2021-05-08 17:00:18 -0400

    I'm starting to sound like a broken record

--- Assets/PhraseList.cs
+++ Assets/PhraseList.cs
@@ -1,4 +1,4 @@
-﻿public static class PhraseList
+public static class PhraseList
 {
     public static string[] phrases =
     {
@@ -75,7 +75,7 @@
         "Moonstone", "Sunstone", "Battery", "Potion", "Stepladder", "Cheat Code", "Trophy", "Crystal Ball",
         "Sunstone", "Battery", "Potion", "Stepladder", "Cheat Code", "Trophy", "Crystal Ball", "Moonstone",
         "Forget Me Not",
-        "Bit Bit", "Crank the Lever", "Primal Rage", "Hang Them", "Literature Club", "Colorblind Mode", "Hell", "Balanced Ternary",
+        "Bit Bit", "Crank the Lever", "Primal Rage", "Hang Them", "Literature Club", "Colorblind Mode", "R’lyehian", "Balanced Ternary",
         "Two Bits", "Silly Slots", "Hunting", "Mafia", "Poetry", "Visual Impairment", "Necronomicon", "Simon Stores",
         "Silly Slots", "Hunting", "Mafia", "Poetry", "Visual Impairment", "Necronomicon", "Simon Stores", "Two Bits",
         "Hunting", "Mafia", "Poetry", "Visual Impairment", "Necronomicon", "Simon Stores", "Two Bits", "Silly Slots",
